3f0229bb885cad5dc20274fac3cb818fa3d4e432
Huncho Jack <meniman98@gmail.com>
2021-03-23 12:25:48 +0100

    implemented touchDelegate

--- app/src/main/java/com/example/thermostat/MainActivity.kt
+++ app/src/main/java/com/example/thermostat/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.thermostat
 
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.TouchDelegate
+import android.view.View
+import com.marcinmoskala.arcseekbar.ArcSeekBar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var seekBar: ArcSeekBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.temperature)
+        seekBar = findViewById(R.id.seekBar)
+
+        val parent = seekBar.parent as View
+        val delegateArea = Rect()
+
+        parent.post {
+            seekBar.getHitRect(delegateArea)
+
+            delegateArea.top += 500
+            delegateArea.bottom += 500
+
+            parent.touchDelegate = TouchDelegate(delegateArea, seekBar)
+        }
+
     }
 }
