c468c571ce7b8d3278d6dcc728f822b1ad59384f
Huncho Jack <meniman98@gmail.com>
2021-04-10 18:09:00 +0200

    creating repo

--- app/src/main/java/com/example/thermostat/network/ApiRequest.kt
+++ app/src/main/java/com/example/thermostat/network/ApiRequest.kt
@@ -1,4 +1,23 @@
 package com.example.thermostat.network
 
-class ApiRequest {
+import retrofit2.Response
+import java.io.IOException
+
+abstract class ApiRequest {
+
+
+    suspend fun <T: Any> apiRequest(call: suspend () -> Response<T>) : T {
+        val response = call.invoke()
+        if (response.isSuccessful) {
+            return response.body()!!
+        }
+        else {
+            //handle the error
+            throw ApiException(response.code().toString())
+
+        }
+    }
+
 }
+
+class ApiException(message: String): IOException(message)
