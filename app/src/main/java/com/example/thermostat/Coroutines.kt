ebd20816a7fe7c8256cb1d8dacf11b872efa8e7e
Huncho Jack <meniman98@gmail.com>
2021-04-10 18:04:47 +0200

    built coroutines

--- app/src/main/java/com/example/thermostat/Coroutines.kt
+++ app/src/main/java/com/example/thermostat/Coroutines.kt
@@ -1,2 +1,19 @@
 package com.example.thermostat
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+object Coroutines {
+
+    fun <T: Any> ioMain(work: suspend( () -> T? ),
+                        callback: ( (T?) -> Unit )) =
+        CoroutineScope(Dispatchers.Main).launch {
+            val data = CoroutineScope(Dispatchers.IO).async rt@{
+                return@rt work()
+            }.await()
+            callback(data)
+        }
+
+}
