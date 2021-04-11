8bb0ac319d0b07b9820721ca5f5c480b0da3b86a
Huncho Jack <meniman98@gmail.com>
2021-04-11 16:18:33 +0200

    created the api request in java

--- app/src/main/java/com/example/thermostat/dataSource/ApiRequest.java
+++ app/src/main/java/com/example/thermostat/dataSource/ApiRequest.java
@@ -1,11 +1,13 @@
 package com.example.thermostat.dataSource
 
 import com.example.thermostat.model.Bungalow;
+import com.example.thermostat.network.BungalowService;
 import com.example.thermostat.presenter.ApiListener;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ApiRequest {
@@ -17,7 +19,42 @@ public class ApiRequest {
 
     public ApiRequest(ApiListener apiListener) {
         this.apiListener = apiListener;
-        callList =
+        callList = BungalowService.getBungalow().getAllBungalows();
+        callBungalow = BungalowService.getBungalow().getSingleBungalow();
+    }
+
+    public void getSingleBungalow() {
+        callBungalow.enqueue(new Callback<Bungalow>() {
+            @Override
+            public void onResponse(Call<Bungalow> call, Response<Bungalow> response) {
+                Bungalow bungalow = response.body();
+
+                apiListener.onSuccess(bungalow);
+            }
+
+            @Override
+            public void onFailure(Call<Bungalow> call, Throwable t) {
+                apiListener.onError(t);
+
+            }
+        });
+    }
+
+
+    public void getAllBungalows() {
+        callList.enqueue(new Callback<List<Bungalow>>() {
+            @Override
+            public void onResponse(Call<List<Bungalow>> call, Response<List<Bungalow>> response) {
+                List<Bungalow> bungalows = response.body();
+
+                apiListener.onSuccess(bungalows);
+            }
+
+            @Override
+            public void onFailure(Call<List<Bungalow>> call, Throwable t) {
+                apiListener.onError(t);
+            }
+        });
     }
 }
 
